cae155b88aea0eafb355b03d714a1c0cbed23b67
Your Name <you@example.com>
2024-07-06 00:36:53 +0300

    done

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -9,11 +9,19 @@ import { AuthService } from '../auth.service';
 export class ProfileComponent implements OnInit {
   username: string | undefined;
   userId: string | undefined;
-  userType: string | null = null; // Initialize userType to null or a default value
+  userType: string | null = null;
+  userProfilePhoto: string | undefined; // User profile photo URL
+  isPhotoChanged: boolean = false;
+  isFormEdited: boolean = false;
+  isEditing: boolean = false; // Flag to track editing mode
 
   constructor(private authService: AuthService) {}
 
   ngOnInit(): void {
+    this.loadProfileData(); // Load profile data on component initialization
+  }
+
+  loadProfileData(): void {
     const decodedToken = this.authService.decodeUserToken();
     if (decodedToken) {
       if (decodedToken.role === 'user') {
@@ -24,6 +32,45 @@ export class ProfileComponent implements OnInit {
         this.userId = decodedToken.id;
       }
       this.userType = decodedToken.role;
+
+      // Load user profile photo from localStorage if available
+      const storedPhoto = localStorage.getItem('userProfilePhoto');
+      if (storedPhoto) {
+        this.userProfilePhoto = storedPhoto;
+        this.isPhotoChanged = true;
+      }
+    }
+  }
+
+  toggleEditing(): void {
+    this.isEditing = !this.isEditing;
+    if (!this.isEditing) {
+      this.isFormEdited = false; // Reset form edited flag when editing is turned off
+    }
+  }
+
+  saveChanges(): void {
+    // Logic to save changes (e.g., update user profile)
+    // After saving, disable editing mode
+    this.isEditing = false;
+    this.isFormEdited = false; // Reset form edited flag
+  }
+
+  onFormEdited(): void {
+    this.isFormEdited = true;
+  }
+
+  onPhotoSelected(event: any): void {
+    const file: File = event.target.files[0];
+    if (file) {
+      const reader: FileReader = new FileReader();
+      reader.onload = (e) => {
+        this.userProfilePhoto = e.target?.result as string;
+        this.isPhotoChanged = true; // Set flag to true when photo is changed
+        // Store photo URL in localStorage
+        localStorage.setItem('userProfilePhoto', this.userProfilePhoto);
+      };
+      reader.readAsDataURL(file);
     }
   }
 }
