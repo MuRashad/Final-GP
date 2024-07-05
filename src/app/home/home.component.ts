a8aa2631f2bbac46a5dadb66a5364f5fcc844394
MuRashad <mustafarashad078@gmail.com>
2024-07-05 19:52:05 +0300

    DOne

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,22 @@
+import { Component, ElementRef, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+  constructor(private elementRef: ElementRef) {}
+
+  ngOnInit(): void {}
+
+  scrollToContact(event: Event): void {
+    event.preventDefault(); // Prevent default action (e.g., navigation)
+
+    const contactSection = document.getElementById('contactSection');
+    if (contactSection) {
+      contactSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  }
+
+}
